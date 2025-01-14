1837198f6565c681d7bf3635f2366eb73a3c3cd1
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-01-14 11:44:52 -0600

    Simuladores Chapter4

---
+++ codigoJava/PrincipalEquals.java
@@ -0,0 +1,14 @@
+class Dog{}
+
+class Cat{}
+
+public class PrincipalEquals {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		
+		System.out.println(dog.equals(cat));
+		
+	}
+}
