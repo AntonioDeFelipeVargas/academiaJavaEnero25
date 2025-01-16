00a80b5999e3836d46eb3eb4c2dcd4094d2d031f
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-01-16 09:04:06 -0600

    Pregunta 10 Capitulo 3

--- Chapter03/src/com/curso/v0/Question10.java
+++ Chapter03/src/com/curso/v0/Question10.java
@@ -1,5 +1,30 @@
 package com.curso.v0;
 
+import java.time.DayOfWeek;
+
 public class Question10 {
+	
+	private DayOfWeek getWeekDay(int day, final int thursday) {
+		
+		int otherDay = day;
+		final int Sunday = 0;
+		final int jueves = 99;
+		
+		switch(otherDay) {
+			default:
+			case 1: return DayOfWeek.MONDAY; //1
+			//case thursday: return DayOfWeek.THURSDAY; //2
+			case jueves:
+			case 2,10: break; 
+			case Sunday: return DayOfWeek.SUNDAY; //3
+			//case DayOfWeek.MONDAY: return DayOfWeek.MONDAY;//4
+		}
+				
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
