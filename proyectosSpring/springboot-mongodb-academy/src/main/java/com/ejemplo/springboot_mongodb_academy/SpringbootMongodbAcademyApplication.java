cc9210b0289ac2ec77fbd1bccb84db82adbae238
Miguel Rugerio <miguel.rugerio@outlook.com>
2025-02-13 10:34:58 -0600

    Se habilito Swagger en el proyecto

--- proyectosSpring/springboot-mongodb-academy/src/main/java/com/ejemplo/springboot_mongodb_academy/SpringbootMongodbAcademyApplication.java
+++ proyectosSpring/springboot-mongodb-academy/src/main/java/com/ejemplo/springboot_mongodb_academy/SpringbootMongodbAcademyApplication.java
@@ -3,7 +3,10 @@ package com.ejemplo.springboot_mongodb_academy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootMongodbAcademyApplication {
 
 	public static void main(String[] args) {
